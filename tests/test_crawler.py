a2f8d6b12bcd8949465f2c2eb1bb7e543cfe7786
Carlos Hernandez <carlos_e8246@hotmail.com>
2023-02-21 14:30:30 -0600

    update test

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,7 +1,17 @@
 from hackafor_crawler_amz import crawler
+from rich.console import Console
 
-URLS = "https://a.co/d/geQhA2O,https://a.co/d/d650BRG,https://a.co/d/5GjLWQx,https://a.co/d/cewgOFz"
-print(crawler.scrap_urls(URLS))
+console = Console()
+
+URLS = ("https://a.co/d/geQhA2O",
+        "https://a.co/d/d650BRG",
+        "https://a.co/d/5GjLWQx",
+        "https://a.co/d/cewgOFz",
+        "https://a.co/d/3h52kBL")
+
+
+def test_crawler():
+    console.print(crawler.scrap_urls(URLS))
 
 # urls = ["https://a.co/d/geQhA2O", "https://a.co/d/d650BRG",
 #         "https://a.co/d/5GjLWQx", "https://a.co/d/cewgOFz"]
